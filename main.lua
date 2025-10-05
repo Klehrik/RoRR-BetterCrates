e8d4182f4f7495ed8736d5e2d5d086aa6e69ac69
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-10-05 15:35:57 -0400

    rewrite

--- main.lua
+++ main.lua
@@ -1,176 +1,136 @@
 -- Better Crates
 -- Klehrik
 
-mods["MGReturns-ENVY"].auto()
-mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
+mods["LuaENVY-ENVY"].auto()
+mods["ReturnsAPI-ReturnsAPI"].auto{
+    namespace   = "betterCrates",
+    mp          = true
+}
 
 local item
-local packet_selection
+local packet
+local saved_selections = {}
 
-local saved = {}
-
-
-
--- ========== Main ==========
-
-Initialize(function()
+local function init()
+    hotloaded = true
+    
     -- Add Cancel item
-    item = Item.new("betterCrates", "cancel", true)
-    item:set_sprite(Resources.sprite_load("betterCrates", "cancel", _ENV["!plugins_mod_folder_path"].."/sCancel.png", 1, 16, 16))
-    item:toggle_loot(false)
-
+    item = Item.new("cancel")
+    item:set_sprite(Sprite.new("cancel", "~/sCancel.png", 1, 16, 16))
 
     -- Packet
-    packet_selection = Packet.new()
-    packet_selection:onReceived(function(message, player)
-        local crate = message:read_instance()
-        local selection = message:read_ushort()
+    packet_free_actor = Packet.new()
+    packet_free_actor:set_serializers(
+        function(buffer, actor)
+            buffer:write_instance(actor)
+        end,
 
-        crate.selection = selection
+        function(buffer, player)
+            local actor = buffer:read_instance()
 
-        -- [Host]  Send to all players
-        if Net.is_host() then
-            local message = packet_selection:message_begin()
-            message:write_instance(crate)
-            message:write_ushort(selection)
-            message:send_exclude(player)
-        end
-    end)
+            GM.actor_activity_set(actor, 0)
 
-
-    local function add_callback(obj)
-        obj:onStep(function(inst)
-            local instData = inst:get_data()
-            local actor = inst.activator
-
-            if inst.active == 0 then
-                instData.loaded_selection = nil
-                instData.prev_selection = -1
-
-            -- Cancel crate selection if Cancel is selected
-            elseif inst.active >= 3 then
-                -- Save selection
-                if Player.get_client():same(actor) then
-                    saved[inst.__object_index] = inst.selection
-                end
-
-                if inst.contents:get(inst.selection) == item.object_id then
-                    inst.active = 0
-
-                    -- Hide item UI
-                    inst.last_move_was_mouse = true
-                    inst.owner = -4
-
-                    -- Free actor activity
-                    actor.activity = 0.0
-                    actor.activity_free = true
-                    actor.activity_move_factor = 1.0
-                    actor.activity_type = 0.0
-                end
-
+            -- [Host]  Send to all players
+            if Net.host then
+                packet_free_actor:send_exclude(player, actor)
             end
-        end)
-    end
+        end
+    )
 
-
-    -- Add callback to vanilla crates
-    local count = gm.array_length(gm.variable_global_get("custom_object"))
-    for id = 0, count - 1 do
-        local obj = Object.wrap(id + Object.CUSTOM_START)
+    -- Add callback to all crates
+    for id = 0, #Global.custom_object - 1 do
+        local obj = Object.wrap(Object.CUSTOM_START + id)
         if obj.base == gm.constants.oCustomObject_pInteractableCrate then
-            add_callback(obj)
+            
+            Callback.add(obj.on_step, Callback.Priority.BEFORE, function(inst)
+                local inst_data = Instance.get_data(inst)
+                local actor = inst.activator
+                local object_index = inst:get_object_index()
+
+                -- Reset variables
+                if inst.active == 0 then
+                    inst_data.loaded_selection = false
+                    
+                    -- Delete `contents`
+                    inst.contents = nil
+
+
+                -- Item selection UI
+                elseif inst.active == 1 then
+                    local contents = inst.contents
+                    if not contents then return end
+
+                    -- Insert Cancel item
+                    -- `contents` does not exist until this point
+                    if not contents:contains(item.object_id) then
+                        contents:insert(0, item.object_id)
+                    end
+
+                    -- Load saved selection
+                    if  (not inst_data.loaded_selection)
+                    and saved_selections[object_index] then
+                        inst_data.loaded_selection = true
+                        inst.selection = math.min(saved_selections[object_index], #contents - 1)
+                    end
+
+
+                -- Cancel crate selection if Cancel is selected
+                elseif inst.active >= 3 then
+                    local contents = inst.contents
+                    if not contents then return end
+
+                    -- Save selection for this crate type
+                    -- if activator is this game client
+                    if Player.get_local() == actor then
+                        saved_selections[object_index] = inst.selection
+                    end
+
+                    -- Check if current selection is Cancel
+                    if contents:get(inst.selection) == item.object_id then
+                        inst.active = 0
+
+                        -- Hide item UI
+                        inst.last_move_was_mouse = true
+                        inst.owner = -4
+
+                        -- Free actor activity
+                        GM.actor_activity_set(actor, 0)
+                    end
+
+                end
+            end)
+
         end
     end
+end
+
+Initialize.add(Callback.Priority.AFTER, init)
+if hotloaded then init() end
 
 
-    -- Add callback to future crate-type interactables
-    -- as the first onStep callback
-    gm.post_script_hook(gm.constants.object_add_w, function(self, other, result, args)
-        if args[3].value == gm.constants.pInteractableCrate then
-            local obj = Object.wrap(result.value)
-            add_callback(obj)
+Hook.add_pre(gm.constants.net_send_instance_message, function(self, other, result, args)
+    -- Intercept net message that is sent on choosing an item
+    -- (pInteractableCrate_Step_2 line 209)
+    if (args[1].value ~= 14)
+    or (args[3].value ~= 3)
+    or (self.object_index ~= gm.constants.oCustomObject_pInteractableCrate)
+    then return end
+
+    local contents = self.contents
+    if not contents then return end
+    
+    -- Check if current selection is Cancel
+    if contents:get(self.selection) == item.object_id then
+        -- Send signal to free actor activity
+        if      Net.host    then packet_free_actor:send_to_all(args[2].value)
+        elseif  Net.client  then packet_free_actor:send_to_host(args[2].value)
         end
-    end)
+        
+        return false
+    end
 end)
 
-
--- Post-initialize
-
-Initialize(function()
-    local function add_callback(obj)
-        obj:onStep(function(inst)
-            local instData = inst:get_data()
-            local actor = inst.activator
-
-            if inst.active == 1 then
-                if not inst.contents then return end
-
-                -- Insert Cancel item
-                local contents = inst.contents
-                if not contents:contains(item.object_id) then
-
-                    -- Delete existing copies of Cancel
-                    local size = #contents
-                    for i = size - 1, 0, -1 do
-                        if contents:get(i) == item.object_id then
-                            contents:delete(i)
-                        end
-                    end
-
-                    contents:insert(0, item.object_id)
-                end
-
-                if Player.get_client():same(actor) then
-                    -- Sync current crate contents
-                    if not instData.synced then
-                        instData.synced = true
-                        Helper.sync_crate_contents(inst)
-                    end
-
-                    -- Sync selection
-                    if inst.selection ~= instData.prev_selection then
-                        instData.prev_selection = inst.selection
-
-                        local message = packet_selection:message_begin()
-                        message:write_instance(inst)
-                        message:write_ushort(inst.selection)
-                        if      Net.is_host()   then message:send_to_all()
-                        elseif  Net.is_client() then message:send_to_host()
-                        end
-                    end
-
-                    -- Load saved selection if it exists
-                    -- This is intentionally after "Sync selection"
-                    -- for a 1-frame delay; do not move
-                    if  not instData.loaded_selection
-                    and saved[inst.__object_index] then
-                        instData.loaded_selection = true
-                        inst.selection = math.min(saved[inst.__object_index], #contents - 1)
-                    end
-                end
-
-            end
-        end)
-    end
-
-
-    -- Add callback to all crate-type interactables
-    -- as the last onStep callback
-    local count = gm.array_length(gm.variable_global_get("custom_object"))
-    for id = 0, count - 1 do
-        local obj = Object.wrap(id + Object.CUSTOM_START)
-        if obj.base == gm.constants.oCustomObject_pInteractableCrate then
-            add_callback(obj)
-        end
-    end
-end, true)
-
-
-gm.post_script_hook(gm.constants.run_create, function(self, other, result, args)
-    saved = {}
-
-    Alarm.create(function()
-        local hud = Instance.find(gm.constants.oHUD)
-        if hud:exists() then hud.command_draw_id:push(-4) end
-    end, 6)
+Hook.add_post(gm.constants.run_create, function(self, other, result, args)
+    -- Reset saved selections every run
+    saved_selections = {}
 end)
